0a502940ff069c5dff6cb3511ba26908d877e988
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-05-27 02:03:07 -0600

    Set up tailwindcss

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,7 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use('/auth', authRouter);
 
